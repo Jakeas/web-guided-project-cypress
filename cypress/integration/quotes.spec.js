4acd7dd64f2c930845a95308fc5c1e7f7230b859
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-04-22 11:16:50 -0500

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,8 @@
 // write tests here
+describe("Quotes app", () => {
+  beforeEach(() => {
+    // arbitrary code we want running before our tests run
+    cy.visit('http"//localhost:1234');
+  });
+  // here go our tests
+});
