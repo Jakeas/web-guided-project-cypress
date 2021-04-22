f9ab8b6587024f31ceb35dcb86bdac6eed6fbe2f
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-04-22 11:19:54 -0500

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -2,7 +2,10 @@
 describe("Quotes app", () => {
   beforeEach(() => {
     // arbitrary code we want running before our tests run
-    cy.visit('http"//localhost:1234');
+    cy.visit("http://localhost:1234");
   });
   // here go our tests
+  it("sanity test to make sure tests work", () => {
+    expect(1 + 2).to.equal(3);
+  });
 });
