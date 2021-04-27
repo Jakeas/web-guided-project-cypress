c247bf4c3f2dc0c5e16891008107be5ae31f7b2b
Jakeas <JakeASierra@gmail.com>
2021-04-26 22:49:56 -0400

    gutted the app

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,54 +5,33 @@ import axios from 'axios'
 const quotesURL = 'http://localhost:3333/api/quotes'
 
 const initialFormState = {
-  id: '',
-  text: '',
-  author: '',
+ 
 }
 
 export default function Container() {
   ////////////// STATES //////////////
   ////////////// STATES //////////////
   ////////////// STATES //////////////
-  const [quotes, setQuotes] = useState([])
-  const [formValues, setFormValues] = useState(initialFormState)
+  const [] = useState()
+  const [] = useState()
 
   ////////////// NETWORK HELPERS //////////////
   ////////////// NETWORK HELPERS //////////////
   ////////////// NETWORK HELPERS //////////////
   const getQuotes = () => {
-    axios.get(quotesURL)
-      .then(response => {
-        setQuotes(response.data)
-      })
-      .catch(handleError)
+   
   }
 
-  const postQuote = ({ text, author }) => {
-    axios.post(quotesURL, { text, author })
-      .then(res => setQuotes(quotes.concat(res.data)))
-      .catch(handleError)
-      .finally(resetForm)
+  const postQuote = ({ , }) => {
+   
   }
 
-  const putQuote = ({ id, text, author }) => {
-    axios.put(`${quotesURL}/${id}`, { text, author })
-      .then(res => {
-        setQuotes(quotes.map(quote => {
-          return quote.id === id ? res.data : quote
-        }))
-      })
-      .catch(handleError)
-      .finally(resetForm)
+  const putQuote = ({ ,, }) => {
+  
   }
 
-  const deleteQuote = (id) => {
-    axios.delete(`${quotesURL}/${id}`)
-      .then(res => { // eslint-disable-line
-        setQuotes(quotes.filter(quote => quote.id !== id))
-      })
-      .catch(handleError)
-      .finally(resetForm)
+  const deleteQuote = () => {
+   
   }
 
   ////////////// OTHER HELPERS //////////////
@@ -63,14 +42,12 @@ export default function Container() {
     setFormValues({ ...quote })
   }
 
-  const handleError = err => { debugger } // eslint-disable-line
-
   const resetForm = () => setFormValues(initialFormState)
 
   ////////////// SIDE EFFECTS //////////////
   ////////////// SIDE EFFECTS //////////////
   ////////////// SIDE EFFECTS //////////////
-  useEffect(() => getQuotes(), [])
+
 
   return (
     <div className='container'>
@@ -80,17 +57,18 @@ export default function Container() {
           quotes.map((q, i) => (
             <li key={q.id}>
               <div>{q.text} ({q.author})</div>
+
               <button data-cy={`editBtn${i}`} onClick={() => editQuote(q.id)}>Edit</button>
+              
               <button data-cy={`deleteBtn${i}`} onClick={() => deleteQuote(q.id)}>Delete</button>
             </li>
           ))
         }
       </ul>
       <Form
-        values={formValues}
-        setValues={setFormValues}
+        ___
+        ___
         submitHandlers={{ postQuote, putQuote }}
-        reset={resetForm}
       />
     </div>
   )
